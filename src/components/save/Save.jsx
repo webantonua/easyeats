423cb1f782b2b5970afe163b997c36974a407595
Antoha2012pro <webantonwebanton@gmail.com>
2026-07-06 13:19:19 +0200

    made no more section "Save"

---
+++ src/components/save/Save.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const cards = [
+    {
+        id: "1",
+        monoTitle: "",
+
+    },
+]
+
+const Save = () => {
+  return (
+    <section>
+      <div className="site-container">
+        <div className="">
+            <h2></h2>
+            <p></p>
+        </div>
+        <div className="">
+
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Save;
---
+++ src/components/save/SaveCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SaveCards = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SaveCards
